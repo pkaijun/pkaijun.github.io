454d31c15a70216fbf411789d2f5ff60faea4444
pkaijun <kai_jun96@hotmail.com>
2018-03-12 00:57:01 +0800

    Update

--- js/popup.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementsByClassName("popup");
-
-for (var i = 0; i < btn.length; i++) {
-  var thisBtn = btn[i];
-  thisBtn.addEventListener("click", function(){
-    var modal = document.getElementById(this.dataset.modal);
-    modal.style.display = "block";
-}, false);
